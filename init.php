e21ac51897ea41628e35f4d76491bc428ad8e9c5
Jansen Price <sumpygump@gmail.com>
2019-02-28 20:07:46 -0600

    Add autoload compatibility with composer global require

--- init.php
+++ init.php
@@ -1,13 +1,29 @@
 <?php
 
-// Autoloader
-if (file_exists(__DIR__ . '/vendor.phar')) {
-    $autoload = require_once __DIR__ . '/vendor.phar';
-} elseif (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    $autoload = require_once __DIR__ . '/vendor/autoload.php';
-} else {
-    die("Autoloader not found. Run `composer install`\n");
+// Find where the composer autoload is
+// This tool was installed as a composed dependency or directly
+$root = realpath(__DIR__);
+$autoloadLocations = array(
+    $root . '/../../autoload.php',
+    $root . '/vendor/autoload.php'
+);
+foreach ($autoloadLocations as $file) {
+    if (file_exists($file)) {
+        define('COMPOSER_AUTOLOAD', $file);
+        break;
+    }
 }
+
+// Composer autoload require guard
+if (!defined('COMPOSER_AUTOLOAD')) {
+    die(
+        "You must run the command `composer install` from the terminal "
+        . "in the directory '$root' before using this tool.\n"
+    );
+}
+
+// Load composer autoloader
+$autoload = require_once COMPOSER_AUTOLOAD;
 
 $app = [];
 
